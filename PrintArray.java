b920468e8957453208044080b26f72c488ac60e6
Komal-8080 <shindeg163@gmail.com>
2021-01-12 20:06:05 +0530

    Added: creating array of integer, double and character

--- PrintArray.java
+++ PrintArray.java
@@ -2,6 +2,7 @@ package dumyproject;
 
 public class PrintArray {
 
+	//Creating Integer Array
 	public static void toPrint(Integer[] inputArray) {
 		for (int element : inputArray) {
 			System.out.printf("%s ", element);
@@ -9,13 +10,15 @@ public class PrintArray {
 		System.out.println();
 	}
 
+	//Creating Double Array
 	public static void toPrint(Double[] inputArray) {
 		for (double element : inputArray) {
 			System.out.printf("%s", element);
 		}
 		System.out.println();
 	}
-
+	
+	////Creating Character Array
 	public static void toPrint(Character[] inputArray) {
 		for (char element : inputArray) {
 			System.out.printf("%s", element);
@@ -23,6 +26,7 @@ public class PrintArray {
 		System.out.println();
 	}
 
+	//Main Method
 	public static void main(String args[]) {
 		Integer[] intArray = { 1, 2, 3, 4, 5 };
 		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
